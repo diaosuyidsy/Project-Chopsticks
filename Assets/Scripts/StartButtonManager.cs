562b4080466c6c5fc7e574a881628d88477e3a58
Tui0yurong <40423557+Tui0yurong@users.noreply.github.com>
2022-01-23 22:50:48 +0800

    提交了fmod restart 结束所有声音

--- Assets/Scripts/StartButtonManager.cs
+++ Assets/Scripts/StartButtonManager.cs
@@ -9,15 +9,18 @@ public class StartButtonManager : MonoBehaviour
 {
     public GameObject startBtn;
     public string sceneName;
+    private FMOD.Studio.Bus MasterBus;
   
     // Start is called before the first frame update
     void Start()
     {
         startBtn.GetComponent<Button>().onClick.AddListener(OnStartBtnClicked);
+        MasterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
     }
 
     void OnStartBtnClicked()
     {
+        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
