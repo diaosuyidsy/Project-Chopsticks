941b3036df34c1a0dd63f9cf0a8806f51a3c4c53
Tui0yurong <40423557+Tui0yurong@users.noreply.github.com>
2022-01-23 22:23:30 +0800

    提交了timermanager

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class TimerManager : MonoBehaviour
 {
-
-    public static int timerValue = 100;
+    public int timerValue = 100;
     public ScoreManager player1Score;
     public ScoreManager player2Score;
 
@@ -14,11 +13,10 @@ public class TimerManager : MonoBehaviour
     public Text player2Text;
     public GameObject blur;
     public GameObject startbutton;
-    
-    
+
 
     private Text time;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,37 +32,40 @@ public class TimerManager : MonoBehaviour
 
     IEnumerator CountTime()
     {
-        while (timerValue >0)
+        while (timerValue > 0)
         {
             timerValue--;
             yield return new WaitForSecondsRealtime(1f);
         }
+
         blur.SetActive(true);
 
-        
+        EventManager.Instance.TriggerEvent(new End());
         //here add finish game logic
         if (player1Score.scoreValue > player2Score.scoreValue)
         {
-
             player1Text.text = "Win";
             player2Text.text = "Lose";
-            
-        }else if (player1Score.scoreValue < player2Score.scoreValue)
+            player1Text.color = Color.green;
+            player2Text.color = Color.red;
+        }
+        else if (player1Score.scoreValue < player2Score.scoreValue)
         {
             player2Text.text = "Win";
             player1Text.text = "Lose";
+            player2Text.color = Color.green;
+            player1Text.color = Color.red;
         }
         else
         {
-            player2Text.text = "Win";
-            player1Text.text = "Win";
+            player2Text.text = "Draw";
+            player1Text.text = "Draw";
+            player1Text.color = Color.yellow;
+            player2Text.color = Color.yellow;
         }
+
         player1Score.enabled = false;
         player2Score.enabled = false;
         startbutton.SetActive(true);
-        
-        
-        
-
     }
 }
