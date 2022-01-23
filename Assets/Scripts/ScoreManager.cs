c5cb6855b0139429c1fb4e1c9fd117ee6bc81f1e
ruofusun <ruofu.sun@gmail.com>
2022-01-23 14:11:45 +0800

    add score ui and update table cloth

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int scoreValue = 0;
+
+    private Text score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = scoreValue.ToString();
+    }
+
+
+    public void AddScore(int score)
+    {
+        scoreValue += score;
+    }
+
+
+
+}
